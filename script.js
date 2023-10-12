ab77f9ccd7b01779f7bd903d0dbfed772a4d2cac
dippio <tylerbatkin@gmail.com>
2023-10-12 19:13:07 +1000

    update things

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // normally id avoid using javascript but its kinda needed here given i cant write decentt css
 function isMobileDevice() {
-    return window.innerWidth <= 768
+    return window.innerWidth <= 580
     // return /iPhone|Android/i.test(navigator.userAgent);
 }
 if (isMobileDevice()) {
